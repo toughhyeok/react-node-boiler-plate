a514f0c7349e2433aa9db9d0170d9d4b54cdb372
junhyeok <toughhyeok96@gmail.com>
2022-10-26 00:41:54 +0900

    [FEAT] sign up post

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json
+app.use(bodyParser.json())
 
 require('dotenv').config()
 
@@ -13,6 +20,16 @@ mongoose.connect(DB_URI, {
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
